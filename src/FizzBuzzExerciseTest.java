b77e7e4c7fbb67816b12cb691d6fd5cfa68afceb
seafruit <18829290140@163.com>
2017-07-27 23:43:47 +0800

    FizzBuzz exercise

---
+++ src/FizzBuzzExerciseTest.java
@@ -0,0 +1,8 @@
+
+public class FizzBuzzExerciseTest {
+
+    public static void main(String[] args) {
+        FizzBuzzExercise fizzBuzzExercise = new FizzBuzzExercise();
+        fizzBuzzExercise.FizzBuzz();
+    }
+}
